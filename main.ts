17c28d33b477a8e11aec3ccbaa200093917d6a6e
Patrik Lindefors <patrik@lindefors.com>
2022-05-16 22:19:53 +0200

    Started

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Coders Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
@@ -26,7 +26,7 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Nisse!');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -38,11 +38,20 @@ export default class MyPlugin extends Plugin {
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
+			id: 'Code block',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				const selection = editor.getSelection();
+				const hljs = require('highlight.js/lib/common');
+				console.log(hljs);
+				const highlight = hljs.highlightAuto(selection, ["java", "javascript", "sql", "kotlin", "python", "groovy", "xml", "html", "yaml", "typescript"]);
+				const language = highlight.language;
+				console.log(highlight);
+				const sel = selection.length == 0 ? '\n' : selection;
+				const lang = selection.length == 0 || language == undefined ? '' : language;
+				console.log(sel);
+
+				editor.replaceSelection('```' + lang + '\n' + sel + '\n```' + '\n');
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
