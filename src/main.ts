8db1b06aac5dece8077cd5fdb4ac753610167c2c
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-10-18 17:09:44 +0900

    Customized the orignal template so that it fits my needs more

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import { Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+		await this.saveSettings();
+		this.addSettingTab(new SampleSettingTab(this));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+
+class SampleSettingTab extends PluginSettingTab {
+	constructor(public plugin: MyPlugin) {
+		super(plugin.app, plugin);
+	}
+
+	display(): void {
+		const {containerEl} = this;
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
