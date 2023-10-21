7dd19110426be5186b346ca2d938788eb7af1044
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-10-21 16:17:56 +0900

    separate settings to another file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
-import { Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin } from 'obsidian';
+import { DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab } from './settings';
 
 
 export default class MyPlugin extends Plugin {
@@ -29,28 +21,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-
-class SampleSettingTab extends PluginSettingTab {
-	constructor(public plugin: MyPlugin) {
-		super(plugin.app, plugin);
-	}
-
-	display(): void {
-		const {containerEl} = this;
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
---
+++ src/settings.ts
@@ -0,0 +1,33 @@
+import { PluginSettingTab, Setting } from 'obsidian';
+import MyPlugin from './main';
+
+
+export interface MyPluginSettings {
+	mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export class SampleSettingTab extends PluginSettingTab {
+	constructor(public plugin: MyPlugin) {
+		super(plugin.app, plugin);
+	}
+
+	display(): void {
+		const {containerEl} = this;
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
