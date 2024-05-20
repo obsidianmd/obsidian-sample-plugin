38e205718ef653e4b7b9f4b61ad0b805f1d6f497
bonndevoff <12user12b12@gmail.com>
2024-05-20 00:52:31 -0300

    NyanBar

--- main.ts
+++ main.ts
@@ -1,134 +1,137 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+function getCheckboxCompletionPercentage(view: MarkdownView): number {
+    const checkboxes = view.contentEl.querySelectorAll<HTMLInputElement>('input[type="checkbox"]');
+    const totalCheckboxes = checkboxes.length;
+    let checkedCount = 0;
 
-interface MyPluginSettings {
-	mySetting: string;
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            checkedCount++;
+        }
+    });
+
+    if (totalCheckboxes === 0) {
+        return 0; // Return 0 if there are no checkboxes in the note
+    }
+
+    return Math.round((checkedCount / totalCheckboxes) * 100);
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class NyanBar extends Plugin {
+    async onload() {
+        this.registerEvent(
+            this.app.workspace.on('file-open', (file) => {
+                const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (activeView) {
+                    this.setupCheckboxListeners(activeView);
+                }
+            })
+        );
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		setInterval(()=>{
+			this.app.workspace.activeEditor?.editor?.refresh()
+		},200)
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+        this.registerMarkdownCodeBlockProcessor('nyanbar', async (source, el) => {
+            const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+            if (!markdownView) return;
+            const view = markdownView;
+            if (source.trim() === 'auto') {
+				// Calculate completion percentage based on checkboxes
+				const completionPercentage = getCheckboxCompletionPercentage(view);
+				let div = el.createEl('div')
+				div.style.display = 'flex'
+				div.style.width = '750px'
+				div.style.height = '70px'
+				let rainbowCont = div.createEl('div')
+				rainbowCont.style.backgroundColor = 'red'
+				rainbowCont.style.backgroundImage = "url('./src/nyanbow.gif')"
+				rainbowCont.style.backgroundSize = completionPercentage + '% 100%' // Ajuste del tamaño del fondo
+				rainbowCont.style.width = '100%' // Asegurarse de que el contenedor ocupe todo el espacio
+			} else {
+                if (source.trim() === '0') {
+                    el.createEl('p').textContent = '0%';
+                    return;
+                }
+                if (parseInt(source.trim()) && parseInt(source.trim()) <= 100 && parseInt(source.trim()) >= 0) {
+                    el.createEl('p').textContent = Math.min(100, Math.max(0, Number(source.trim()))) + '%';
+                } else {
+					el.createEl('p').textContent = source
 				}
-			}
-		});
+            }
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.addRibbonIcon('cat', 'NyanBar', () => {
+            // Called when the user clicks the icon.
+            new Notice(`${this.manifest.name}\nMade by ${this.manifest.author}\nv${this.manifest.version}`);
+        });
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // This adds a simple command that can be triggered anywhere
+        this.addCommand({
+            id: 'add-nyanbar',
+            name: 'Add NyanBar',
+            hotkeys: [{ modifiers: ['Mod', 'Shift'], key: 'b' }],
+            editorCallback: (editor: Editor) => {
+                editor.setLine(editor.getCursor().line, '```nyanbar\nauto\n```');
+            },
+        });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SettingTab(this.app, this));
+    }
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    setupCheckboxListeners(view: MarkdownView) {
+        const checkboxes = view.contentEl.querySelectorAll<HTMLInputElement>('input[type="checkbox"]');
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener('change', () => {
+				this.registerMarkdownCodeBlockProcessor('nyanbar', async (source, el) => {
+					const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+					if (!markdownView) return;
+					const view = markdownView;
+					if (source.trim() === 'auto') {
+						// Calculate completion percentage based on checkboxes
+						const completionPercentage = getCheckboxCompletionPercentage(view);
+						el.createEl('p').textContent = completionPercentage + '%';
+					} else {
+						if (source.trim() === '0') {
+							el.createEl('p').textContent = '0%';
+							return;
+						}
+						if (parseInt(source) && parseInt(source) <= 100 && parseInt(source) >= 0) {
+							el.createEl('p').textContent = Math.min(100, Math.max(0, Number(source))) + '%';
+						} else {
+							el.createEl('p').textContent = source
+						}
+					}
+				});
+                this.app.workspace.trigger('markdown:reprocess', view);
+            });
+        });
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+    plugin: NyanBar;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: NyanBar) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('NyanBar')
+            .setDesc('Track your progress with NyanBar.');
 
-	display(): void {
-		const {containerEl} = this;
+			containerEl.createEl('h6').textContent = `Run "/add-nyanbar" then place a number from "0" to "100" or do "auto" for automatic progress according to the note's checked checkboxes.`;
+			let e = containerEl.createEl('p')
+			e.style.color = '#FF1445'
+			e.textContent = 'WARNING: The progress bar update is WIP, switch between notes to update progress bar for now, if you know about obsidian API and want to help us finish the development of this Tool, please message @bonndubz on discord.'
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		}
 }
