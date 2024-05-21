9d6e8a35ae8d49d703cc7bf15328b35a9e1347b2
bonndevoff <12user12b12@gmail.com>
2024-05-21 16:26:20 -0300

    1.0.0

--- main.ts
+++ main.ts
@@ -205,7 +205,7 @@ class SettingTab extends PluginSettingTab {
 
 			containerEl.createEl('img').src = 'https://github.com/xhyabunny/obsidian-sample-plugin/assets/106491722/262b2c04-c5bc-44a3-86d0-26967b9b4660'
 			
-			containerEl.createEl('h6').textContent = `Run "/add-nyanbar" or "/add-pusheenbar" then place a number from "0" to "100" or do "auto" for automatic progress according to the note's checked checkboxes.`;
+			containerEl.createEl('h6').textContent = `Run "/nyan" or "/pusheen" then place a number from "0" to "100" or do "auto" for automatic progress according to the note's checked checkboxes.`;
 			let e = containerEl.createEl('p')
 			e.addClass('err')
 			e.textContent = 'WARNING: The progress bar update is WIP, switch between notes to update progress bar for now, if you know about obsidian API and want to help us finish the development of this Tool, please message @bonndubz on discord.'
