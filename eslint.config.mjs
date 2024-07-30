4c794f807ef44559e5a9aa29eff2455e03583e4e
Peter Strøiman <peter@stroiman.com>
2024-07-30 14:30:31 +0200

    Update packages, and migrate eslint configuration
    This updates the eslint configuration to the new flat file format, which
    is the future format.
    
    Also adds eslint as a dev dependency. JS Projects should have locally
    installed the correct versions of development tools like
    eslint/prettier, etc. So developer experience is consistent; and not
    depending on external context, like globally installed versions; but the
    version that matches the configuration.
    
    Also updates outdated packages, like typescript
    
    Would have liked to update to eslint@9, but the typescript module does
    not yet support 9 as a peer dependency.

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/node_modules/", "**/main.js"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-unused-vars": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            args: "none",
+        }],
+
+        "@typescript-eslint/ban-ts-comment": "off",
+        "no-prototype-builtins": "off",
+        "@typescript-eslint/no-empty-function": "off",
+    },
+}];
