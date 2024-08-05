9f888670d14f18cc444d82da3c6b890487ae1422
Peter Strøiman <peter@stroiman.com>
2024-08-05 16:31:28 +0200

    Replace TS eslint with `typescript-eslint`
    This replaces the older @typescript-eslint/* packages with new flat-file
    aware configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
 import globals from "globals";
-import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
 	{
 		ignores: ["**/node_modules/", "**/main.js"],
 	},
-	eslint.configs.recommended,
+	...tseslint.configs.recommended,
 	{
 		plugins: {
 			"@typescript-eslint": tseslint.plugin,
 		},
-
 		languageOptions: {
 			globals: {
 				...globals.node,
@@ -21,17 +19,14 @@ export default tseslint.config(
 			ecmaVersion: 5,
 			sourceType: "module",
 		},
-
 		rules: {
 			"no-unused-vars": "off",
-
 			"@typescript-eslint/no-unused-vars": [
 				"error",
 				{
 					args: "none",
 				},
 			],
-
 			"@typescript-eslint/ban-ts-comment": "off",
 			"no-prototype-builtins": "off",
 			"@typescript-eslint/no-empty-function": "off",
