2af50b85cfebfc359fcc9500623507178545e50e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-08-27 19:56:23 +0200

    Create src/page/SampleSettingTab.ts

---
+++ src/page/SampleSettingTab.ts
@@ -0,0 +1,30 @@
+import { PluginSettingTab, App, Setting } from "obsidian";
+import MyPlugin from "./main";
+
+export class SampleSettingTab extends PluginSettingTab {
+  plugin: MyPlugin;
+
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName("Setting #1")
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your secret")
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          }),
+      );
+  }
+}
