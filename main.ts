cd96ed0bf22b42134e098a374c9e98f7df659b53
kitelev <kitelev.andrey@gmail.com>
2024-12-30 15:44:42 +0500

    Add command

--- main.ts
+++ main.ts
@@ -4,8 +4,18 @@ export default class ExoPlugin extends Plugin {
 
 	async onload() {
 		this.addRibbonIcon('dice', 'Exo', () => {
-			new Notice('This is Exocortex!');
+			this.showModal();
 		});
+
+		this.addCommand({
+            id: 'exo-first-notice',
+            name: 'Shows simple Notice',
+            callback: () => {this.showModal()}
+        })
+	}
+
+	async showModal(): Promise<void> {
+		new Notice('This is Exocortex!');
 	}
 
 	onunload() {
