382422eb1ca6aa936412a1db4acfca5a1644e6e3
Your Name <you@example.com>
2025-05-06 11:15:37 +0900

    First commit

--- main.ts
+++ main.ts
@@ -1,134 +1,131 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
+// 設定インターフェースの定義
 interface MyPluginSettings {
-	mySetting: string;
+	fleetingFolder: string;
+	archiveFolder: string;
+	daysThreshold: number;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	fleetingFolder: 'Fleeting',  // デフォルトのFleetingフォルダ名
+	archiveFolder: 'Archives',   // デフォルトのArchivesフォルダ名
+	daysThreshold: 7,            // デフォルトの期間（7日）
 }
 
-export default class MyPlugin extends Plugin {
+export default class AutoMoveFleetingNotesPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// コマンドを追加
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: 'move-fleeting-to-archive',
+			name: 'Move Fleeting Notes to Archive',
+			callback: () => this.moveFleetingNotes(),
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// 設定タブを追加
+		this.addSettingTab(new AutoMoveFleetingNotesSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// その他のプラグイン機能
 	}
 
-	onunload() {
-
-	}
-
+	// 設定のロード
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
+	// 設定の保存
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	// Fleetingノートを移動する機能
+	async moveFleetingNotes() {
+		const { fleetingFolder, archiveFolder, daysThreshold } = this.settings;
+		const now = Date.now();
+
+		// ファイルを取得
+		const files = this.app.vault.getFiles();
+
+		for (const file of files) {
+			// Fleetingフォルダにあるノートを対象
+			if (file.path.startsWith(fleetingFolder)) {
+				const created = this.getCreatedDate(file);  // created:: メタデータを取得
+				if (created && now - created > daysThreshold * 86400000) {
+					// 期間が経過している場合、移動
+					const newPath = file.path.replace(fleetingFolder, archiveFolder);
+					await this.app.vault.rename(file, newPath);  // ファイルを移動
+					console.log(`Moved ${file.path} to ${newPath}`);
+				}
+			}
+		}
+	}
+
+	// created:: メタデータを取得
+	getCreatedDate(file: TFile): number | null {
+		const metadata = this.app.metadataCache.getFileCache(file);
+		const created = metadata?.frontmatter?.created;
+		if (created && !isNaN(Date.parse(created))) {
+			return Date.parse(created);
+		}
+		return null;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// 設定タブの作成
+class AutoMoveFleetingNotesSettingTab extends PluginSettingTab {
+	plugin: AutoMoveFleetingNotesPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AutoMoveFleetingNotesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		// Fleetingフォルダ名の設定
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Fleeting Folder')
+			.setDesc('The folder where Fleeting notes are stored.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter Fleeting folder name')
+				.setValue(this.plugin.settings.fleetingFolder)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.fleetingFolder = value;
 					await this.plugin.saveSettings();
+				}));
+
+		// Archivesフォルダ名の設定
+		new Setting(containerEl)
+			.setName('Archive Folder')
+			.setDesc('The folder where Fleeting notes will be moved to.')
+			.addText(text => text
+				.setPlaceholder('Enter Archive folder name')
+				.setValue(this.plugin.settings.archiveFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.archiveFolder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// 移動までの日数の設定
+		new Setting(containerEl)
+			.setName('Days Threshold')
+			.setDesc('The number of days after which Fleeting notes will be moved to the Archive.')
+			.addText(text => text
+				.setPlaceholder('Enter number of days')
+				.setValue(this.plugin.settings.daysThreshold.toString())
+				.onChange(async (value) => {
+					const days = parseInt(value);
+					if (!isNaN(days)) {
+						this.plugin.settings.daysThreshold = days;
+						await this.plugin.saveSettings();
+					}
 				}));
 	}
 }
