90290a8af5bd9c7b0d43418c5c06cfd319ecb295
Johannes Theiner <j.theiner@live.de>
2025-07-03 12:00:06 +0200

    Update to use released eslint plugin

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import tseslint from 'typescript-eslint';
+import obsidianmd from "eslint-plugin-obsidianmd";
+import globals from "globals";
+
+export default tseslint.config(
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	...obsidianmd.configs.recommended,
+
+);
