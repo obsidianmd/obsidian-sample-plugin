123fc4a8705c066132693de3ec0024800b2a595f
Johannes Theiner <j.theiner@live.de>
2025-11-14 14:28:33 +0100

    update eslint plugin version remove builtin-modules external dependency

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from "builtin-modules";
+import { builtinModules } from 'node:module';
 
 const banner =
 `/*
@@ -31,7 +31,7 @@ const context = await esbuild.context({
 		"@lezer/common",
 		"@lezer/highlight",
 		"@lezer/lr",
-		...builtins],
+		...builtinModules],
 	format: "cjs",
 	target: "es2018",
 	logLevel: "info",
